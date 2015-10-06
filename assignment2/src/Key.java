8923804ede0eebe68764520b34e635c0f63cd281
Eirik S. Norheim <noreirik@gmail.com>
2015-10-07 01:35:04 +0200

    Allow for distinguishing between creating private and public keys

--- assignment2/src/Key.java
+++ assignment2/src/Key.java
@@ -1,9 +1,28 @@
-public abstract class Key {
+public class Key {	
 	private double n;
-	private int de;
+	private int de; //d for private key, e for public key
 	
-	public Key() {
-		this.generateKey();
+	private Key(double n) {
+		this.setN(n);
 	}
-	protected abstract void generateKey();
+	
+	public static Key generatePublicKey(double n, double totient) {
+		Key pubKey = new Key(n);
+		int e;
+		do {
+			e = Algorithms.getRandomNumber();
+		} while (!Algorithms.isRelativePrime(totient, e));
+		pubKey.setDE(e);
+		return pubKey;
+	}
+	public static Key generatePrivateKey(double n, int e, double totient) {
+		Key privateKey = new Key(n);
+		// find d as modular inverse of e (mod totient); set as de
+		return privateKey;
+	}
+	
+	public int getDE() { return de; }
+	public double getN() { return n; }
+	public void setDE(int de) { this.de = de; }
+	public void setN(double n) { this.n = n; }
 }
--- assignment2/src/KeyPair.java
+++ assignment2/src/KeyPair.java
@@ -1,7 +1,15 @@
-
 public class KeyPair {
 	private Key privateKey;
 	private Key publicKey;
+	
+	public KeyPair() {
+		int p = Algorithms.getRandomPrime();
+		int q = Algorithms.getRandomPrime();
+		double n = p * q;
+		double totient = Algorithms.getTotient(p, q);
+		publicKey = Key.generatePublicKey(n,totient);
+		privateKey = Key.generatePrivateKey(n, publicKey.getDE(),totient);
+	}
 	
 	public Key getPrivateKey() { return privateKey; }
 	public Key getPublicKey() { return publicKey; }
